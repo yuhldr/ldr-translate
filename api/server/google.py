4940f2721b7dede2993b348053dc2126c3c8ca92
yuhldr <yuhldr@qq.com>
2022-09-14 18:53:16 +0800

    谷歌翻译bug

--- api/server/google.py
+++ api/server/google.py
@@ -7,6 +7,7 @@ config_server = server_config.server_google
 
 
 def translate_text(s, fromLang="auto", to_lang_code="zh-cn"):
+    s = s.replace("#", "")
     print("谷歌" + to_lang_code)
     url = 'https://translate.googleapis.com/translate_a/single?'
     param = 'client=gtx&dt=t&sl=%s&tl=%s&q=%s' % (fromLang, to_lang_code, s)
